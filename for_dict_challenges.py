2532fde87b54e18b470e481a797c126f31a3c5ed
Artem Kuznetsov <759167@mail.ru>
2019-12-03 23:03:26 +0300

    Completed tasks for and dict

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -53,17 +53,21 @@ school_students = [
     {'first_name': 'Оля'},
   ]
 ]
-name = []
-for first_name in school_students:
-    for value in first_name:
-        name.append(value['first_name'])
-value_names, count_names = Counter(name).most_common(1)[0]
-print(f'Самое частое имя среди учеников: {value_names}')
+
+numb_of_class = 0
+for classes in school_students:
+    numb_of_class += 1
+    name_of_class = []
+    for name in classes:
+        name_of_class.append(name['first_name'])
+    value_names, count_names = Counter(name_of_class).most_common(1)[0]
+    print(f'Самое частое имя в классе {numb_of_class}: {value_names}')
 
 # Пример вывода:
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
 
+print()
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
@@ -77,12 +81,22 @@ is_male = {
   'Олег': True,
   'Миша': True,
 }
-# ???
+for sch_class in school:
+    male = 0
+    female = 0
+    for children_name in sch_class['students']:
+        if is_male[children_name['first_name']]:
+            male += 1
+        else:
+            female += 1
+    numb_of_class = sch_class['class']
+    print(f'В классе {numb_of_class} {female} девочки и {male} мальчика')
 
 # Пример вывода:
 # В классе 2a 2 девочки и 0 мальчика.
 # В классе 3c 0 девочки и 2 мальчика.
 
+print()
 
 # Задание 5
 # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
@@ -96,7 +110,21 @@ is_male = {
   'Олег': True,
   'Миша': True,
 }
-# ???
+count_male = {}
+count_female = {}
+for sch_class in school:
+    male = 0
+    female = 0
+    for children_name in sch_class['students']:
+        if is_male[children_name['first_name']]:
+            male += 1
+        else:
+            female += 1
+    count_male[sch_class['class']] = male
+    count_female[sch_class['class']] = female
+
+print('Больше всего мальчиков в классе {}'.format(max(count_male, key=count_male.get)))
+print('Больше всего девочек в классе {}'.format(max(count_female, key=count_female.get)))
 
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
